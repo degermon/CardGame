c060516d1625a47682231238509909c17da81df3
degermon <degermon@gmail.com>
2020-03-04 20:14:24 +0200

    Added simple background with gradient

--- CardGame/Controllers/MainViewController.swift
+++ CardGame/Controllers/MainViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet var backgroundView: UIView!
+    
+    private var currentGameDifficulty: Bool = false // false is easy, true is hard
+    private let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBackground()
     }
     
-    // - MARK: Actions
+    override func viewWillAppear(_ animated: Bool) {
+        selectBackgroundColor()
+    }
+    
+    // MARK: - Config
+    
+    func setBackground() {
+        gradientLayer.frame = view.bounds
+        gradientLayer.shouldRasterize = true
+        // Diagonal: top left to bottom corner.
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0) // Top left corner.
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1) // Bottom right corner.
+        // Horizontal: left to right.
+        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func selectBackgroundColor() {
+        if currentGameDifficulty == false {
+            gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1).cgColor]
+        } else {
+            gradientLayer.colors = [#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).cgColor, #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor]
+        }
+    }
+    
+    // MARK: - Actions
 
     @IBAction func settingsButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "showSettings", sender: sender)
--- CardGame/Controllers/SettingsViewController.swift
+++ CardGame/Controllers/SettingsViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBOutlet var backgroundView: UIView!
+    
+    private let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setBackground()
     }
     
+    // MARK: - Config
+    
+    func setBackground() {
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [#colorLiteral(red: 0.7956927419, green: 0.4451861978, blue: 0.8532453179, alpha: 1).cgColor, #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0) // Top left corner.
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1) // Bottom right corner.
+        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+    }
 
     /*
     // MARK: - Navigation
